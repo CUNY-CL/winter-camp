b95b547e9dcc60081648d5e52a2d05f172b3c128
degan <dennis.egan@siriusxm.com>
2022-07-07 12:59:51 -0400

    updated year to 2007

--- get_dataset.sh
+++ get_dataset.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget http://data.statmt.org/news-crawl/en/news.2019.en.shuffled.deduped.gz
-tar -xf news.2019.en.shuffled.deduped.gz
+wget http://data.statmt.org/news-crawl/en/news.2007.en.shuffled.deduped.gz
+gzip -dk news.2007.en.shuffled.deduped.gz
