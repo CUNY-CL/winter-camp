8c892ded7c3ca10e77a40ef21a732d3f1386cbb0
degan <dennis.egan@siriusxm.com>
2022-07-07 13:23:56 -0400

    Created script.sh to re-create experiment

---
+++ script.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+mkdir submission
+mkdir submission/data
+mkdir submission/data/news
+mkdir submission/data/dril
+
+# Download corpus
+cd submission/data
+chmod +x get_dataset.sh
+../../get_dataset.sh
+cd ../../
+
+# Pip install module
+pip install -e .
+
+# Train model on news corpus
+# Set --max_examples to 1,000,000 just so things run quickly (my laptop is not the strongest...)
+python caseify/train.py \
+ --filepath submission/data/news.2007.en.shuffled.deduped \
+ --directory submission/data/news --max_examples 100000
+
+# Train twitter model on @dril tweets
+python caseify/tweets.py \
+  --bearer_token AAAAAAAAAAAAAAAAAAAAAAcpegEAAAAAVGCIJ%2Fv%2BRHcBPK4%2FDSNtN29yc34%3DUUXrgRcyHDHA9PRbEOciT5MB6IKsV8ByAef62UPhZSdaMMhz1i \
+  --user dril \
+  --clean 1 \
+  --directory submission/data/dril \
+  --pretrained submission/data/news/model.pkl
